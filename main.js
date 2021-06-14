63440f28a5437e912eb52a16f850c919ef63f172
caroline <pcmaunda@gmail.com>
2021-06-14 10:40:30 +0300

    all modified files

--- main.js
+++ main.js
@@ -15,3 +15,8 @@ jQuery('h1').click(function(){
       $("img").show();
     });
   });
+
+  $("ul#list li:first").css("color","red");// selecting  the first item on the list //
+  $("ul#list li:last").css("color","red");// selecting the last item on the list//
+  $("ul#list li:even").css("background-color", "yellow"); //changing the background color for even nubmers//
+  $("ul#list li:odd").css("background-color","#cccccc");// changing background color  for odd numbers//
